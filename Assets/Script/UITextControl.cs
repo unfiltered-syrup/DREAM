e94073b458189b2c0713deaef99dada7f26fbc7a
Evets Hu <stevehu@alterstaff.io>
2026-02-01 10:51:44 -0500

    handshake fix

--- Assets/Script/UITextControl.cs
+++ Assets/Script/UITextControl.cs
@@ -28,7 +28,7 @@ public class EyeContactUI : MonoBehaviour
         if (_playerLogic != null && _textComponent != null)
         {
             float timeValue = _playerLogic.EyeContactDuration;
-            _textComponent.text = $"Cringe\nLV:{Mathf.RoundToInt(timeValue).ToString()}";
+            // _textComponent.text = $"Cringe\nLV:{Mathf.RoundToInt(timeValue).ToString()}";
         }
     }
 }
